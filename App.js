9b6728bc48f8d82e7eb9c234fb0bbceef462f3d7
Prudhvikoganti <40680570+Prudhvikoganti@users.noreply.github.com>
2020-10-19 11:48:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Timeline from './components/Timeline';
+
+import './App.css';
+
+const App = () => {
+   return (<>
+    <h1>React Timeline</h1>
+    <Timeline />
+    </>
+   )
+};
+
+export default App;
---
+++ data.js
@@ -0,0 +1,69 @@
+const data = [
+    {
+        text: 'React Components and Props',
+        data: 'October 19 2020',
+        category:{
+            tag: 'easy',
+            color: 'green'
+        },
+        link:{
+            url:
+                'https://reactjs.org/docs/components-and-props.html',
+            text: 'Read more'
+        }
+    },
+    {
+        text: 'React State and Lifecycle',
+        data: 'October 20 2020',
+        category:{
+            tag: 'Hard',
+            color: 'red'
+        },
+        link:{
+            url:
+                'https://reactjs.org/docs/state-and-lifecycle.html',
+            text: 'Read more'
+        }
+    },
+    {
+        text: 'React Higher-Order Components',
+        data: 'October 21 2020',
+        category:{
+            tag: 'medium',
+            color: 'orange'
+        },
+        link:{
+            url:
+                'https://reactjs.org/docs/higher-order-components.html',
+            text: 'Read more'
+        }
+    },
+    {
+        text: 'React Hooks',
+        data: 'October 22 2020',
+        category:{
+            tag: 'hard',
+            color: 'red'
+        },
+        link:{
+            url:
+                'https://reactjs.org/docs/hooks-intro.html',
+            text: 'Read more'
+        }
+    },
+    {
+        text: 'React Handling Events',
+        data: 'October 23 2020',
+        category:{
+            tag: 'medium',
+            color: '#018f69'
+        },
+        link:{
+            url:
+                'https://reactjs.org/docs/handling-events.html',
+            text: 'Read more'
+        }
+    }
+];
+
+export default data;
